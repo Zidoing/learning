47850671e275db0bd08f6c987551536a954a6166
zhoulei <403907871@qq.com>
2017-05-19 17:55:01 +0800

    page 42

---
+++ python_cookbook/chapter1_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @DateTime : 
+# @Author   : Z LEI
+
+
+print '13213'.isdigit()  # 纯数字
+print '1323fdasf13'.isalnum()  # 字符数字
+print 'sssss'.isalpha()  # 纯字符
+print 'sssx'.islower()  # 小写
+print ' '.isspace()  # 是否是空
+print 'AAAA'.isupper()  # 是否大写
+print 'Title'.istitle()  # 是否开头为大写
---
+++ python_cookbook/readme.md
@@ -0,0 +1,1 @@
+# python cookbook 第二版本
