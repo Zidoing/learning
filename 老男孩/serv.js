e2d55c70d029411c30df5f5742da53435ce685ba
zhoulei <403907871@qq.com>
2017-11-08 10:25:50 +0800

    c

---
+++ 老男孩/serv.js
@@ -0,0 +1,16 @@
+/**
+ * Created by zhoulei on 07/11/2017.
+ */
+
+
+
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('hello world');
+
+}).listen(8888);
+
+
+console.log('server running at http://127.0.0.1:8888/');
